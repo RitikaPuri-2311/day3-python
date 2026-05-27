d19d1866626a52ef8a574bebaa611aa993880e2b
Ritika <ritika@codecolonies.com>
2026-05-27 21:44:01 +0530

    Find most common element in list

---
+++ Exercise/find_most_common.py
@@ -0,0 +1,14 @@
+#find the most common element in a list 
+
+from collections import Counter
+
+list = ["Cup", "Cup", "Cup", "Coffee", "Coffee", "Tea"]
+
+l1 = Counter(list)
+result = l1.most_common()
+print(result)
+result2 = l1.most_common(1)
+print(result2)
+
+result3 = Counter("elephantinzoo").most_common(1)
+print(result3)
